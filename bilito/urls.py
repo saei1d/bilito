b9d839e8edcd48818a6e7d45114d1c6ccca6c1c5
saei1d <hehehe3bar@gmail.com>
2023-11-22 11:56:05 +0330

    init

--- bilito/urls.py
+++ bilito/urls.py
@@ -13,7 +13,6 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-    jasdjfkladshvohgshguewfh
 """
 from django.contrib import admin
 from django.urls import path
@@ -21,6 +20,3 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-#;lokijuhgfdfghjklkjhg
-
-#sdfghjklkjhgfds
